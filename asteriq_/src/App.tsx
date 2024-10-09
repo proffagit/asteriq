e37b61331b62a8d745557cb6a1232b1d926e1989
Obed <e2101936@edu.vamk.fi>
2024-10-09 20:20:10 +0300

    basic layout

--- asteriq_/src/App.tsx
+++ asteriq_/src/App.tsx
@@ -1,35 +1,50 @@
 import './App.css'
 import { IonIcon } from '@ionic/react'
-import { documentOutline } from 'ionicons/icons'
+import { document, folder, search, arrowForward } from 'ionicons/icons'
 
 function App() {
-  
-  const alexanderTheGreatQuotes = [
-      "I am not afraid of an army of lions led by a sheep; I am afraid of an army of sheep led by a lion.",
-      "There is nothing impossible to him who will try.",
-      "A tomb now suffices him for whom the world was not enough.",
-      "I would rather live a short life of glory than a long one of obscurity.",
-      "In the end, when it's over, all that matters is what you've done.",
-      "He who is not a good servant will not be a good master.",
-      "I am indebted to my father for living, but to my teachers for living well.",
-      "The greatest victory is that which requires no battle.",
-      "To conquer the world is not enough; it is necessary to be able to conquer oneself.",
-      "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.",
-      "I would rather die of passion than of boredom.",
-      "The weak fall, but the strong will remain and never go under!",
-      "There is nothing more important than the freedom of the mind.",
-      "A great man is one who can have power and not abuse it.",
-      "What we do in life echoes in eternity.",
-      "I am not a man, I am a legend."
-  ];
-
   return (
-    <div>
-      <div className="Quotes">
-        <h4>{alexanderTheGreatQuotes[Math.floor(Math.random() * alexanderTheGreatQuotes.length)]}</h4>
+
+    <div className="main-container">
+    <div className="menu-container">
+
+      {/* SEARCH BAR */}
+      <div className="search-container">
+        <IonIcon icon={search} className="search-icon" />
+        <input type="text" placeholder="Search..." className="search-input" />
+        <IonIcon icon={arrowForward} className="search-arrow-icon" />
       </div>
-      <IonIcon icon={documentOutline} />
+
+      {/* <h5 className="recent">Recenly opened</h5> */}
+      <div className="centered-container">
+        <div className="icon-name-container">
+          <IonIcon icon={folder} className="large-icon" />
+          <p className="name">study</p>
+        </div>
+
+        <div className="icon-name-container">
+          <IonIcon icon={document} className="large-icon" />
+          <p className="name">recipes</p>
+        </div>
+
+        <div className="icon-name-container">
+          <IonIcon icon={folder} className="large-icon" />
+          <p className="name">poems</p>
+        </div>
+
+        <div className="icon-name-container">
+          <IonIcon icon={document} className="large-icon" />
+          <p className="name">random</p>
+        </div>
+      </div>
     </div>
+
+
+    <div className="content-container">
+
+      content here
+    </div>
+  </div>
   )
 }
 
